504f57b1ede566fd913edbbc1435ec1697ac7336
Andy McBlane <andymcblane@hotmail.com>
2022-07-10 19:06:37 +1000

    Fix indent

--- code/main.py
+++ code/main.py
@@ -27,8 +27,8 @@ COLLECTION_URL = os.environ["COLLECTION_URL"]
 
 
 def process_browser_log_entry(entry):
-	response = json.loads(entry['message'])['message']
-	return response
+    response = json.loads(entry['message'])['message']
+    return response
 
 
 d = DesiredCapabilities.CHROME
@@ -61,73 +61,73 @@ time.sleep(2)
 
 queued_downloads = []
 try:
-	while remaining:
-		driver.get(COLLECTION_URL)
-		episode_container = driver.find_element("xpath", "//div[@class='container']")
-		episodes = episode_container.find_elements("xpath", "//div[@class='row-wrapper row-with-margin']")
+    while remaining:
+        driver.get(COLLECTION_URL)
+        episode_container = driver.find_element("xpath", "//div[@class='container']")
+        episodes = episode_container.find_elements("xpath", "//div[@class='row-wrapper row-with-margin']")
 
-		try:
-			episode = episodes[index]
-		except:
-			remaining = False
-			break 
+        try:
+            episode = episodes[index]
+        except:
+            remaining = False
+            break 
 
-		title = episode.text
-		title = title.split("\n")
-		for row in title:
-			frags = row.split("|")
-			if len(frags) > 0:
-				if len(frags) == 2:
-					event_name = frags[0]
-					episode_name = frags[1]
-				elif len(frags) == 3:
-					event_name = frags[0]
-					episode_name = frags[2]
-				break
-		print(episode_name)
-		# something weird happens when the element to click is /just/ on the page
-		if failed:
-			driver.find_element("xpath", "//body").send_keys(Keys.END)
-			time.sleep(5)
-			failed = False
-		try:
-			# if the page needs to scroll, the element won't be cliclable right away
-			episode.click()
-		except ElementClickInterceptedException:
-			time.sleep(2)
-			episode.click()
+        title = episode.text
+        title = title.split("\n")
+        for row in title:
+            frags = row.split("|")
+            if len(frags) > 0:
+                if len(frags) == 2:
+                    event_name = frags[0]
+                    episode_name = frags[1]
+                elif len(frags) == 3:
+                    event_name = frags[0]
+                    episode_name = frags[2]
+                break
+        print(episode_name)
+        # something weird happens when the element to click is /just/ on the page
+        if failed:
+            driver.find_element("xpath", "//body").send_keys(Keys.END)
+            time.sleep(5)
+            failed = False
+        try:
+            # if the page needs to scroll, the element won't be cliclable right away
+            episode.click()
+        except ElementClickInterceptedException:
+            time.sleep(2)
+            episode.click()
 
 
-		# the video player will (possibly?) select the best quality for the connection. Give it time to select the best
-		time.sleep(20) 
+        # the video player will (possibly?) select the best quality for the connection. Give it time to select the best
+        time.sleep(20) 
 
-		# get network logs 
-		browser_log = driver.get_log('performance') 
-		events = [process_browser_log_entry(entry) for entry in browser_log]
-		events = [event for event in events if 'Network.responseReceived' in event['method']]
+        # get network logs 
+        browser_log = driver.get_log('performance') 
+        events = [process_browser_log_entry(entry) for entry in browser_log]
+        events = [event for event in events if 'Network.responseReceived' in event['method']]
 
-		last_event = ""
-		for event in events:
-			# print(event)
-			if "response" in event["params"]:
-				# assuming the video player will only improve quality, get the last record for the m3u8 
-				if ".mp4.m3u8" in event["params"]["response"]["url"]:
-					last_event = event["params"]["response"]["url"]
-		if last_event != "":
-			queued_downloads.append(["yt-dlp", "-o", f"{WRITE_LOCATION}{event_name.strip()}/{episode_name.strip()}.mp4", last_event]) 
-		else:
-			failed = True
-			failed_count+=1
-			if failed_count > 5:
-				# can't download this episode - why? Have seen some just don't load. 
-				index+=1
-				failed = False
-			continue
-				
-		driver.back()
-		time.sleep(5) # give the back time to do it's thing. We will re-load the page anyway
-		index+=1
+        last_event = ""
+        for event in events:
+            # print(event)
+            if "response" in event["params"]:
+                # assuming the video player will only improve quality, get the last record for the m3u8 
+                if ".mp4.m3u8" in event["params"]["response"]["url"]:
+                    last_event = event["params"]["response"]["url"]
+        if last_event != "":
+            queued_downloads.append(["yt-dlp", "-o", f"{WRITE_LOCATION}{event_name.strip()}/{episode_name.strip()}.mp4", last_event]) 
+        else:
+            failed = True
+            failed_count+=1
+            if failed_count > 5:
+                # can't download this episode - why? Have seen some just don't load. 
+                index+=1
+                failed = False
+            continue
+                
+        driver.back()
+        time.sleep(5) # give the back time to do it's thing. We will re-load the page anyway
+        index+=1
 finally:
-	driver.quit()
+    driver.quit()
 for item in queued_downloads:
-	subprocess.call(item)
+    subprocess.call(item)
